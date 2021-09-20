bdbc81a2e615c11683bbbd1242146c45fd6db703
Nine <pattadhonmt@gmail.com>
2021-09-20 20:43:10 +0700

    Initial commit

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,33 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FLUTTER FOOD'),
+        backgroundColor: Colors.green.shade900,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+
+              Text('THIS IS A HOME PAGE',style: TextStyle(fontSize: 50.0),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login/login_page.dart
@@ -0,0 +1,284 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../HomePage.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+Widget _showci() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      width: 40.0,
+      height: 40.0,
+      //color: Colors.green, // ห้ามกำหนด color ตรงนี้ ถ้าหากกำหนดใน BoxDecoration แล้ว
+      decoration: BoxDecoration(
+        color: Colors.green.shade100,
+        shape: BoxShape.circle,
+      ),
+    ),
+  );
+}
+
+Widget _showciblack() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Container(
+      width: 40.0,
+      height: 40.0,
+      //color: Colors.green, // ห้ามกำหนด color ตรงนี้ ถ้าหากกำหนดใน BoxDecoration แล้ว
+      decoration: BoxDecoration(
+        color: Colors.green.shade900,
+        shape: BoxShape.circle,
+      ),
+    ),
+  );
+}
+
+Widget _sh = Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    _showci(),_showci(),_showci(),_showci(),_showci(),_showci()
+  ],
+);
+
+setshow(int i) {
+  if (i == 1) {
+    _sh = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _showciblack(),_showci(),_showci(),_showci(),_showci(),_showci()
+      ],
+    );
+  }
+  else if(i == 2){
+    _sh = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _showciblack(),_showciblack(),_showci(),_showci(),_showci(),_showci()
+      ],
+    );
+  }else if(i == 3){
+    _sh = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _showciblack(),_showciblack(),_showciblack(),_showci(),_showci(),_showci()
+      ],
+    );
+  }else if(i == 4){
+    _sh = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _showciblack(),_showciblack(),_showciblack(),_showciblack(),_showci(),_showci()
+      ],
+    );
+  }else if(i == 5){
+    _sh = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _showciblack(),_showciblack(),_showciblack(),_showciblack(),_showciblack(),_showci()
+      ],
+    );
+  }else if(i == 6){
+    _sh = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _showciblack(),_showciblack(),_showciblack(),
+        _showciblack(),_showciblack(),_showciblack()
+      ],
+    );
+  }else if(i == 0){
+    _sh = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _showci(),_showci(),_showci(),_showci(),_showci(),_showci()
+      ],
+    );
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  var input = '';
+  String showtext = '123456';//รหัสที่ถูกต้อง
+
+  void _showError() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('ERROR'),
+          content: Text('Invalid PIN. Please try again.'),
+          actions: [
+            // ปุ่ม OK ใน dialog
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                // ปิด dialog
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            // ไล่เฉดจากมุมบนซ้ายไปมุมล่างขวาของ Container
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            // ไล่เฉดจากสีแดงไปสีน้ำเงิน
+            colors: [
+              Colors.brown,
+              Colors.black,
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.lock_outline,
+                        size: 100.0,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        'LOGIN',
+                        style: Theme.of(context).textTheme.headline1,
+                      ),
+                      /*Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          input,
+                          style: TextStyle(fontSize: 30.0, color: Colors.white),
+                        ),
+                      ),*/
+                      Expanded(
+                        child: _sh,
+
+                      ),
+
+
+
+
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                //color: Colors.green.shade700,
+                child: Column(
+                  children: [
+                    [1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9],
+                    [-2, 0, -1],
+                  ].map((row) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: row.map((item) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: LoginButton(
+                            number: item,
+                            onClick: _handleClickButton,
+                          ),
+                        );
+                      }).toList(),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleClickButton(int num) {
+    print(input.length);
+    setState(() {
+      if (num == -1) {
+        if (input.length > 0) input = input.substring(0, input.length - 1);
+        setshow(input.length);
+      } else {
+        input = input + '$num';
+        setshow(input.length);
+      }
+    });
+    if (input.length == 6) {
+      if (input == showtext) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+        setshow(0);
+        input =('');
+      } else {
+        _showError();
+        input = '';
+        setshow(0);
+      }
+    }
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final int number;
+  final Function(int) onClick;
+
+  const LoginButton({
+    required this.number,
+    required this.onClick,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      customBorder: CircleBorder(),
+      onTap: number == -2
+          ? null
+          : () {
+              onClick(number);
+            },
+      child: Container(
+        width: 80.0,
+        height: 80.0,
+        decoration: number == -2
+            ? null
+            : BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white, width: 2.0),
+              ),
+        child: Center(
+          child: number >= 0
+              ? Text('$number', style: Theme.of(context).textTheme.headline6)
+              : (number == -1
+                  ? Icon(
+                      Icons.backspace_outlined,
+                      size: 28.0,
+                      color: Colors.white,
+                    )
+                  : SizedBox.shrink()),
+        ),
+      ),
+    );
+  }
+}
